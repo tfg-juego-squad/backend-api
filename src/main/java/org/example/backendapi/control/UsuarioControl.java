0fb1b9e805e201222e997774ba0e749795e7f411
daaviddieeguez <daaviddieeguez@gmail.com>
2026-03-04 21:23:55 +0100

    Crear controlador REST para la entidad Usuario

---
+++ src/main/java/org/example/backendapi/control/UsuarioControl.java
@@ -0,0 +1,87 @@
+package org.example.backendapi.control;
+
+import org.example.backendapi.model.dao.IUsuarioDAO;
+import org.example.backendapi.model.entities.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/tfg/usuarios")
+public class UsuarioControl {
+    @Autowired
+    IUsuarioDAO usuarioDAO;
+
+    @GetMapping
+    public List<Usuario> buscarUsuarios() {
+        return (List<Usuario>) usuarioDAO.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> buscarUsuarioPorId(@PathVariable(value = "id") String id){
+        Optional<Usuario> usuario = usuarioDAO.findUsuarioById(id);
+        if(usuario.isPresent()){
+            return ResponseEntity.ok().body(usuario.get());
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/nombre/{nombre}")
+    public ResponseEntity<List<Usuario>> buscarUsuariosPorNombre(@PathVariable(value = "nombre") String nombre){
+        List<Usuario> listaUsuarios = usuarioDAO.findUsuarioByNombreUsuario(nombre);
+        if(!listaUsuarios.isEmpty()){
+            return ResponseEntity.ok().body(listaUsuarios);
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/rol/{rol}")
+    public ResponseEntity<List<Usuario>> buscarUsuariosPorRol(@PathVariable(value = "rol") String rol){
+        List<Usuario> listaUsuarios = usuarioDAO.findUsuarioByRol(rol);
+        if(!listaUsuarios.isEmpty()){
+            return ResponseEntity.ok().body(listaUsuarios);
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @Validated
+    @PostMapping("/alta")
+    public Usuario guardarUsuario(@Validated @RequestBody Usuario usuario){
+        return usuarioDAO.save(usuario);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> actualizarUsuario(@RequestBody Usuario nuevoUsuario,
+                                                @PathVariable(value = "id") String id) {
+        Optional<Usuario> usuario = usuarioDAO.findById(id);
+        if (usuario.isPresent()) {
+            usuario.get().setNombreUsuario(nuevoUsuario.getNombreUsuario());
+            usuario.get().setRol(nuevoUsuario.getRol());
+            usuario.get().setFechaCreacion(nuevoUsuario.getFechaCreacion());
+            usuario.get().setHashContrasena(nuevoUsuario.getHashContrasena());
+            usuarioDAO.save(usuario.get());
+            return ResponseEntity.ok().body("Actualizado");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> borrarUsuario (@PathVariable(value = "id") String id) {
+        Optional<Usuario> usuario = usuarioDAO.findUsuarioById(id);
+        if(usuario.isPresent()) {
+            usuarioDAO.deleteById(id);
+            return ResponseEntity.ok().body("Borrado");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
---
+++ src/main/java/org/example/backendapi/model/dao/IUsuarioDAO.java
@@ -0,0 +1,14 @@
+package org.example.backendapi.model.dao;
+
+import org.example.backendapi.model.entities.Usuario;
+import org.springframework.data.repository.CrudRepository;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+
+public interface IUsuarioDAO extends CrudRepository<Usuario, String> {
+    Optional<Usuario> findUsuarioById(String id);
+    List<Usuario> findUsuarioByNombreUsuario(String nombreUsuario);
+    List<Usuario> findUsuarioByRol(String rol);
+}
